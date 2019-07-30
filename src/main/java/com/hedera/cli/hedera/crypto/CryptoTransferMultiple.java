ddc905c9a725de64c998b8c69fa08d464757fd7d
Serene Lim <enerestar@gmail.com>
2019-07-30 22:31:56 +0800

    Allow multiple arguments for crypto transfer recipient list:

--- src/main/java/com/hedera/cli/hedera/crypto/CryptoTransferMultiple.java
+++ src/main/java/com/hedera/cli/hedera/crypto/CryptoTransferMultiple.java
@@ -4,23 +4,28 @@ package com.hedera.cli.hedera.crypto;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.util.Arrays;
+
 @Command(name= "multiple",
         description = "@|fg(magenta) Transfer hbars to multiple accounts|@",
         helpCommand = true)
 public class CryptoTransferMultiple implements Runnable {
 
-    @Option(names = {"-r", "--recipient"}, arity = "0...",
+    @Option(names = {"-r", "--recipient"}, split = " ", arity = "0..*",
             description = "Recipient to transfer to"+
             "%n@|bold,underline Usage:|@%n" +
-            "@|fg(yellow) transfer multiple -r=1234,-a=100 OR%n" +
-            "transfer multiple --recipient=1234,--recipientAmt=100|@")
-    private String recipient;
+            "@|fg(yellow) transfer multiple -r=1001,1002,-a=100,100 OR%n" +
+            "transfer multiple --recipient=1001,1002,--recipientAmt=100,100|@")
+    private String[] recipient;
 
-    @Option(names = {"-a", "--recipientAmt"}, arity = "0...", description = "Amount to transfer")
-    private String recipientAmt;
+    @Option(names = {"-a", "--recipientAmt"}, split = " ", arity = "0..*", description = "Amount to transfer")
+    private String[] recipientAmt;
 
     @Override
     public void run() {
         System.out.println("TODO multiple transfer list");
+        System.out.println(Arrays.asList(recipient));
+        System.out.println(Arrays.asList(recipientAmt));
+        System.out.println(recipient.length + " " + recipientAmt.length);
     }
 }
