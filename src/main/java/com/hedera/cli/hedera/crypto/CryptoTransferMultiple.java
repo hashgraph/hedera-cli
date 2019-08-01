e38ced0c3b4acfbef3bbaa03783fb56ed02df0cb
Serene Lim <enerestar@gmail.com>
2019-08-01 15:30:59 +0800

    Allow multiple recipients to be added for cli, add tests, todo - figure out sdk for multi transfer

--- src/main/java/com/hedera/cli/hedera/crypto/CryptoTransferMultiple.java
+++ src/main/java/com/hedera/cli/hedera/crypto/CryptoTransferMultiple.java
@@ -1,20 +1,29 @@
 package com.hedera.cli.hedera.crypto;
 
-
+import com.hedera.cli.hedera.Hedera;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.account.CryptoTransferTransaction;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import shadow.org.codehaus.plexus.util.StringUtils;
 
+import javax.annotation.Nullable;
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Command(name= "multiple",
         description = "@|fg(magenta) Transfer hbars to multiple accounts|@",
         helpCommand = true)
-public class CryptoTransferMultiple implements Runnable {
+public class CryptoTransferMultiple implements Runnable  {
 
     @Option(names = {"-r", "--recipient"}, split = " ", arity = "0..*",
             description = "Recipient to transfer to"
                     +"%n@|bold,underline Usage:|@%n"
-                    + "@|fg(yellow) transfer multiple -r=1001,1002,-a=100,100|@")
+                    + "@|fg(yellow) transfer multiple -r=1001,1002,1003,-a=100,100,100|@")
     private String[] recipient;
 
     @Option(names = {"-a", "--recipientAmt"}, split = " ", arity = "0..*", description = "Amount to transfer")
@@ -26,5 +35,102 @@ public class CryptoTransferMultiple implements Runnable {
         System.out.println(Arrays.asList(recipient));
         System.out.println(Arrays.asList(recipientAmt));
         System.out.println(recipient.length + " " + recipientAmt.length);
+
+        var recipientList = Arrays.asList(recipient);
+        var amountList = Arrays.asList(recipientAmt);
+        System.out.println(recipientList);
+        System.out.println(amountList);
+        recipientList(recipientList, amountList);
+        var operatorId = Hedera.getOperatorId();
+        var client = Hedera.createHederaClient();
+
+        // TODO figure out sdk for cryptotransfer
+//        new CryptoTransferTransaction(client)
+//                .addSender(operatorId, 100)
+//                .addTransfer();
+
+    }
+
+    public void recipientList(List<String > accountList, List<String> amountList) {
+        AccountId accountId;
+        String acc, amt;
+        Map<Integer, Recipient> map = new HashMap<>();
+        long sum = 0;
+
+        try {
+            System.out.println(accountList);
+            System.out.println(amountList);
+            if (accountList.size() != amountList.size())
+                System.err.println("Lists aren't the same size");
+            else {
+                for (int i = 0; i < accountList.size(); ++i) {
+
+                    acc = accountList.get(i);
+                    amt = amountList.get(i);
+                    var cleanAccString = StringUtils.isNumeric(acc);
+                    if (cleanAccString) {
+                        if (isAccountId(acc) && isNumeric(amt)) {
+                            System.out.println(acc);
+                            accountId = AccountId.fromString("0.0." + acc);
+                            var amount = new BigInteger(amt);
+                            sum += amount.longValue();
+                            Recipient recipient = new Recipient(accountId, amount.longValue());
+                            map.put(i, recipient);
+                        }
+                    }
+                }
+                System.out.println("Total sum is " + sum);
+                map.forEach((key, value) -> {
+                    if (map.size() != amountList.size()) {
+                        throw new IllegalArgumentException("Please check your recipient list");
+                    }
+                    System.out.println(key + ":" + "Acc: " + value.accountId + " Amt: " + value.amount);
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isNumeric(final String str) {
+        // checks null or empty
+        if(str == null || str.isEmpty()) {
+            return false;
+        }
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isAccountId(final String str) {
+        // checks null or empty
+        if(str == null || str.isEmpty()) {
+            return false;
+        }
+
+        if (str.matches("^[0]+$")) {
+            return false;
+        }
+
+        try {
+            AccountId.fromString("0.0." + str);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    private class Recipient {
+
+        private AccountId accountId;
+        private Long amount;
+
+        private Recipient(AccountId accountId, Long amount) {
+            this.accountId = accountId;
+            this.amount = amount;
+        }
     }
 }
---
+++ src/test/java/com/hedera/cli/hedera/crypto/CryptoTransferMultipleTest.java
@@ -0,0 +1,86 @@
+package com.hedera.cli.hedera.crypto;
+
+import com.hedera.hashgraph.sdk.account.AccountId;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import shadow.org.codehaus.plexus.util.StringUtils;
+
+import java.math.BigInteger;
+import java.util.*;
+import org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+
+public class CryptoTransferMultipleTest {
+
+    @Test
+    public void testIsNumeric() {
+        String str = "111111111";
+        CryptoTransferMultiple cryptoTransferMultiple = new CryptoTransferMultiple();
+        assertTrue(cryptoTransferMultiple.isNumeric(str));
+
+        String str1 = " ";
+        CryptoTransferMultiple cryptoTransferMultiple1 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple1.isNumeric(str1));
+
+        String str2 = null;
+        CryptoTransferMultiple cryptoTransferMultiple2 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple2.isNumeric(str2));
+
+        String str3 = "0.1";
+        CryptoTransferMultiple cryptoTransferMultiple3 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple3.isNumeric(str3));
+
+        String str4 = "-9";
+        CryptoTransferMultiple cryptoTransferMultiple4 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple4.isNumeric(str4));
+    }
+
+    @Test
+    public void testIsAccountId() {
+        String str = "1001";
+        CryptoTransferMultiple cryptoTransferMultiple = new CryptoTransferMultiple();
+        assertTrue(cryptoTransferMultiple.isAccountId(str));
+
+        String str1 = " ";
+        CryptoTransferMultiple cryptoTransferMultiple1 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple1.isAccountId(str1));
+
+        String str2 = "10a01";
+        CryptoTransferMultiple cryptoTransferMultiple2 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple2.isAccountId(str2));
+
+        String str3 = "000";
+        CryptoTransferMultiple cryptoTransferMultiple3 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple3.isAccountId(str3));
+
+        String str4 = "-";
+        CryptoTransferMultiple cryptoTransferMultiple4 = new CryptoTransferMultiple();
+        assertFalse(cryptoTransferMultiple4.isAccountId(str4));
+    }
+
+    @Test
+    public void testRecipientList() {
+        List<String> accountList = Arrays.asList("1001", "1002", "1003");
+        List<String> amountList= Arrays.asList("100", "9888486986", "10000001100000");
+        CryptoTransferMultiple cryptoTransferMultiple = new CryptoTransferMultiple();
+        cryptoTransferMultiple.recipientList(accountList, amountList);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testRecipientList1() {
+//        thrown.expect(IllegalArgumentException.class);
+//        thrown.expectMessage("Please check your recipient list");
+//        thrown.reportMissingExceptionWithMessage("Exception expected");
+//        List<String> accountList = Arrays.asList("1001", "1002", "1003");
+//        List<String> amountList= Arrays.asList("100", "9888486986", "1000000 1100000");
+//        CryptoTransferMultiple cryptoTransferMultiple = new CryptoTransferMultiple();
+//        cryptoTransferMultiple.recipientList(accountList, amountList);
+    }
+}
