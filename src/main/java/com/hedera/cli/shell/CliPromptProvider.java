0beb19050f01ed47723b94fbab392c058d63a295
Calvin Cheng <linchuan.cheng@gmail.com>
2019-08-09 13:27:54 +0800

    Show the current network in use in our prompt provider

--- src/main/java/com/hedera/cli/shell/CliPromptProvider.java
+++ src/main/java/com/hedera/cli/shell/CliPromptProvider.java
@@ -1,6 +1,9 @@
 package com.hedera.cli.shell;
 
+import com.hedera.cli.hedera.utils.DataDirectory;
+
 import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStringBuilder;
 import org.jline.utils.AttributedStyle;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,22 @@ public class CliPromptProvider implements PromptProvider {
 
     @Override
     public AttributedString getPrompt() {
-        return new AttributedString("hedera :> ", AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE));
+        // blue
+        AttributedString hederaAttr = new AttributedString("hedera ",
+                AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE));
+
+        // green
+        String currentNetwork = DataDirectory.readFile("network.txt", "aspen");
+        AttributedString currentNetworkAttr = new AttributedString("[" + currentNetwork + "]",
+                AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
+
+        // blue
+        AttributedString promptAttr = new AttributedString(":> ",
+                AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE));
+
+        // one more: current operator account
+
+        AttributedStringBuilder builder = new AttributedStringBuilder();
+        return builder.append(hederaAttr).append(currentNetworkAttr).append(promptAttr).toAttributedString();
     }
 }
