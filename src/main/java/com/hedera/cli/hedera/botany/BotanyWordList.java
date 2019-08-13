9d89c148677a5aa40834e8101ea197a126b80860
Calvin Cheng <linchuan.cheng@gmail.com>
2019-08-13 10:59:43 +0800

    Generate random botany names to be used for our account file json

---
+++ src/main/java/com/hedera/cli/hedera/botany/BotanyWordList.java
@@ -0,0 +1,9 @@
+package com.hedera.cli.hedera.botany;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BotanyWordList {
+  /** https://www.house-plant-hobbyist.com/blog/2018/7/23/botanical-baby-names */
+  static public List<String> words = Arrays.asList("ainsley", "alder", "alyssum");
+}
--- src/main/java/com/hedera/cli/hedera/setup/Setup.java
+++ src/main/java/com/hedera/cli/hedera/setup/Setup.java
@@ -2,14 +2,17 @@ package com.hedera.cli.hedera.setup;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import com.hedera.cli.config.InputReader;
 import com.hedera.cli.hedera.bip39.Mnemonic;
 import com.hedera.cli.hedera.bip39.MnemonicException.MnemonicChecksumException;
 import com.hedera.cli.hedera.bip39.MnemonicException.MnemonicLengthException;
 import com.hedera.cli.hedera.bip39.MnemonicException.MnemonicWordException;
+import com.hedera.cli.hedera.botany.BotanyWordList;
 import com.hedera.cli.hedera.keygen.CryptoUtils;
 import com.hedera.cli.hedera.keygen.EDKeyPair;
+import com.hedera.cli.hedera.utils.DataDirectory;
 
 import org.springframework.stereotype.Component;
 
@@ -33,6 +36,8 @@ public class Setup implements Runnable {
     // System.out.println("Recovering account id " + accountId);
     // TODO: prompt user to input the accountId
 
+    String accountId = inputReader.prompt("account id that we will use as default operator");
+
     String phrase = inputReader.prompt("24 words phrase", "secret", false);
     List<String> phraseList = Arrays.asList(phrase.split(" "));
     System.out.println(phraseList);
@@ -51,8 +56,20 @@ public class Setup implements Runnable {
     } catch (MnemonicLengthException | MnemonicWordException | MnemonicChecksumException e) {
       e.printStackTrace();
     }
-  }
+
 
   // TODO: once done, we write it as an "account_name.json" file and mark the account id in default.txt
+  DataDirectory dataDirectory = new DataDirectory();
+  // String filename = 
+
+  }
+
+  private String getRandomName() {
+    Random rand = new Random();
+    List<String> botanyNames = BotanyWordList.words;
+    // HederaNode node = nodes.get(rand.nextInt(nodes.size()))
+
+  }
+
 
 }
