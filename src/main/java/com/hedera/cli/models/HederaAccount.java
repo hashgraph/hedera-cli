8e5380daac535368cff802c84edce41904241c33
Calvin Cheng <linchuan.cheng@gmail.com>
2019-08-14 16:50:07 +0800

    Add HederaAccount model

---
+++ src/main/java/com/hedera/cli/models/HederaAccount.java
@@ -0,0 +1,23 @@
+package com.hedera.cli.models;
+
+public class HederaAccount {
+  
+  private String accountId;
+
+  private String privateKey;
+
+  private String publicKey;
+
+  public String getAccountId() {
+    return accountId;
+  }
+
+  public String getPrivateKey() {
+    return privateKey;
+  }
+
+  public String getPublicKey() {
+    return publicKey;
+  }
+
+}
