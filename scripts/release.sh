5ecb68b3a918d07d18b415349ff90fb584dec2fd
Calvin Cheng <calvin@hedera.com>
2019-08-31 21:55:27 +0800

    Update homebrew tap formula programmatically
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ while IFS= read -r line
 do
     # display $line or do somthing with $line
     export VERSION="$(echo $line | cut -d'=' -f2)"
-    printf '%s\n' "v$VERSION will be released"
+    printf '%s\n' "v${VERSION} will be released"
 done <"$file"
 
 export GH_USER=hashgraph
@@ -15,13 +15,17 @@ GH_TARGET=master
 ASSETS_PATH=.
 NAME=hedera
 SHA256="$(sha256sum ${NAME}-${VERSION}.tar.gz | cut -d' ' -f1)"
-echo $SHA256 > hash.txt
+PACKAGE="${NAME}-${VERSION}.tar.gz"
 
-tar -zcvf "${NAME}-${VERSION}.tar.gz" .
+tar -zcvf "${PACKAGE}" .
 
 git add -u
 git commit -m "$VERSION release"
 git push
+
+echo "${SHA256}" > hash.txt
+echo "${VERSION}" > version.txt
+echo "${PACKAGE}" > package.txt
 
 res=`curl --user "$GH_USER:$GH_PATH" -X POST https://api.github.com/repos/${GH_USER}/${GH_REPO}/releases \
 -d "
@@ -40,6 +44,10 @@ file_name=${NAME}-${VERSION}.tar.gz
 curl --user "$GH_USER:$GH_PATH" -X POST https://uploads.github.com/repos/${GH_USER}/${GH_REPO}/releases/${rel_id}/assets?name=${file_name}\
  --header 'Content-Type: text/javascript ' --upload-file ${ASSETS_PATH}/${file_name}
 
-rm ${ASSETS_PATH}/${file_name}
-
 source package_homebrew.sh
+
+# clean up
+rm ${ASSETS_PATH}/${file_name}
+rm hash.txt
+rm version.txt
+rm package.txt
