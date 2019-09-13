c6722413a86c996af0b83818365331a5387742d5
Calvin Cheng <calvin@hedera.com>
2019-09-13 15:07:10 +0800

    Test homebrew release automation
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- scripts/package_homebrew.sh
+++ scripts/package_homebrew.sh
@@ -5,7 +5,7 @@ HOMEBREW_TAP_REPO=homebrew-tap
 git clone git@github.com:${GH_USER}/${HOMEBREW_TAP_REPO}.git
 cd ${HOMEBREW_TAP_REPO}
 
-./sha256update.rb "${GH_REPO}"
+./scripts/sha256update.rb "${GH_REPO}"
 
 git add -u
 git commit -m "${GH_REPO} ${VERSION} release"
