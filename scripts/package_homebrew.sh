d32801db27d04883af636116aa5f9929d60fe85f
Calvin Cheng <calvin@hedera.com>
2019-09-13 16:00:12 +0800

    Test homebrew release automation
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- scripts/package_homebrew.sh
+++ scripts/package_homebrew.sh
@@ -7,6 +7,8 @@ cd ${HOMEBREW_TAP_REPO}
 
 ./scripts/sha256update.rb "${GH_REPO}"
 
+git config --global user.name "OSS Hedera"
+git config --global user.email "oss@hedera.com"
 git add -u
 git commit -m "${GH_REPO} ${VERSION} release"
 git push
