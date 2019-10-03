b61fa418dd16e7ee670024708b4ddb837321f9b7
Serene Lim <enerestar@gmail.com>
2019-10-03 11:20:48 +0800

    Use Picoclispringfactory to pass application of springboot into picocli subcommand line, allow constructor arguments to be passed via pico too
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- src/main/java/com/hedera/cli/hedera/crypto/Transfer.java
+++ src/main/java/com/hedera/cli/hedera/crypto/Transfer.java
@@ -1,6 +1,7 @@
 package com.hedera.cli.hedera.crypto;
 
 import com.hedera.cli.config.InputReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -18,20 +19,32 @@ public class Transfer implements Runnable {
         CommandLine.usage(this, System.out);
     }
 
-    public void handle(InputReader inputReader, String subCommand, String... args) {
+    public void handle(ApplicationContext context, InputReader inputReader, String subCommand, String... args) {
         switch (subCommand) {
             case "single":
                 if (args.length == 0) {
                     CommandLine.usage(new CryptoTransfer(inputReader), System.out);
                 } else {
-                    new CommandLine(new CryptoTransfer(inputReader)).execute(args);
+                    try {
+                        PicocliSpringFactory picocliSpringFactory = new PicocliSpringFactory(context);
+                        new CommandLine(new CryptoTransfer(inputReader), picocliSpringFactory).execute(args);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             case "multiple":
                 if (args.length == 0) {
                     CommandLine.usage(new CryptoTransferMultiple(inputReader), System.out);
                 } else {
-                    new CommandLine(new CryptoTransferMultiple(inputReader)).execute(args);
+                    try {
+                        // This way, we can not only able to pass application context of spring boot to picocli via ifactory,
+                        // we can also pass in constructor arguments ie inputreader
+                        PicocliSpringFactory picocliSpringFactory = new PicocliSpringFactory(context);
+                        new CommandLine(new CryptoTransferMultiple(inputReader), picocliSpringFactory).execute(args);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             default:
