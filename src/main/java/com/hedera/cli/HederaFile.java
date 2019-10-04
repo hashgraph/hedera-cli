f3ef5d68feb47487163bdb757ee65d06f193b511
Serene Lim <enerestar@gmail.com>
2019-10-04 14:44:47 +0800

    Ditto for files
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- src/main/java/com/hedera/cli/HederaFile.java
+++ src/main/java/com/hedera/cli/HederaFile.java
@@ -1,10 +1,12 @@
 package com.hedera.cli;
 
+import com.hedera.cli.config.InputReader;
 import com.hedera.cli.defaults.CliDefaults;
 import com.hedera.cli.hedera.file.File;
 import com.hedera.cli.shell.ShellHelper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellMethodAvailability;
@@ -13,17 +15,24 @@ import org.springframework.shell.standard.ShellOption;
 @ShellComponent
 public class HederaFile extends CliDefaults {
 
-  @Autowired
-  ShellHelper shellHelper;
+    @Autowired
+    ShellHelper shellHelper;
 
-  public HederaFile() {}
-  
-  @ShellMethodAvailability("isDefaultNetworkAndAccountSet")
-  @ShellMethod(value = "manage hedera file")
-  public void file(
-          @ShellOption(defaultValue = "") String subCommand,
-          @ShellOption(defaultValue = "", arity = -1) String... args) {
-      File file = new File();
-      file.handle(subCommand, args);
-  }
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    InputReader inputReader;
+
+    public HederaFile() {
+    }
+
+    @ShellMethodAvailability("isDefaultNetworkAndAccountSet")
+    @ShellMethod(value = "manage hedera file")
+    public void file(
+            @ShellOption(defaultValue = "") String subCommand,
+            @ShellOption(defaultValue = "", arity = -1) String... args) {
+        File file = new File();
+        file.handle(context, inputReader, subCommand, args);
+    }
 }
