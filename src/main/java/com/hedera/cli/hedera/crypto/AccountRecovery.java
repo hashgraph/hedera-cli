fb485718721d0eae139b32089e8d69be1e903b44
Calvin Cheng <calvin@hedera.com>
2019-10-14 12:13:24 +0800

    fix missing import
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- src/main/java/com/hedera/cli/hedera/crypto/AccountRecovery.java
+++ src/main/java/com/hedera/cli/hedera/crypto/AccountRecovery.java
@@ -21,6 +21,7 @@ import com.hedera.cli.hedera.utils.AccountUtils;
 import com.hedera.cli.hedera.utils.DataDirectory;
 import com.hedera.cli.hedera.utils.Utils;
 import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.account.AccountInfoQuery;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
 
 import org.hjson.JsonObject;
