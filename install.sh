04324bcf9e2e08fd3f1bc344b7284c5cca76f2f9
Calvin Cheng <calvin@hedera.com>
2019-10-15 10:59:45 +0800

    36 curl installer (#38)
    * General installation script with curl
    
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -
+
+VERSION=`curl -s https://api.github.com/repos/hashgraph/hedera-cli/releases/latest | grep "tag_name" | cut -d '"' -f 4`
+echo "Latest hedera-cli is $VERSION"
+
+DOWNLOAD_URL=`curl -s https://api.github.com/repos/hashgraph/hedera-cli/releases/latest | grep "browser_download_url" | cut -d '"' -f 4`
+FILENAME=`echo ${DOWNLOAD_URL##*/}`
+
+# download tar.gz
+echo "Downloading $FILENAME"
+wget --show-progress $DOWNLOAD_URL -O $FILENAME
+
+# unpack and install into path
+echo "Installing $FILENAME"
+tar -C /usr/local/bin -xvf $FILENAME
+
+# remove downloaded tar.gz
+echo "Cleaning up"
+rm $FILENAME
+
