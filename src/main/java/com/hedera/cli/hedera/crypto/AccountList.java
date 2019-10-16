dd296307f4cc98c9ca0f1192318e5fe383463a25
Serene Lim <enerestar@gmail.com>
2019-10-16 10:46:06 +0800

    Closes #37
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- src/main/java/com/hedera/cli/hedera/crypto/AccountList.java
+++ src/main/java/com/hedera/cli/hedera/crypto/AccountList.java
@@ -1,22 +1,41 @@
 package com.hedera.cli.hedera.crypto;
 
+import com.hedera.cli.hedera.utils.AccountUtils;
+import com.hedera.cli.shell.ShellHelper;
+import org.checkerframework.checker.units.qual.K;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 import picocli.CommandLine.Command;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.hedera.cli.hedera.utils.DataDirectory;
 
+@Component
 @Command(name = "ls", description = "@|fg(225) List of all accounts for the current network.|@")
 public class AccountList implements Runnable {
 
   @Override
   public void run() {
     System.out.println("List of accounts in the current network");
-    // OS-agnostic path to ~/.hedera/[currentNetwork]/accounts
     DataDirectory dataDirectory = new DataDirectory();
-    String currentNetwork = dataDirectory.readFile("network.txt", "aspen");
-    String path = currentNetwork + File.separator + "accounts";
-    dataDirectory.listFiles(path);
-  }
+    AccountUtils accountUtils = new AccountUtils();
+    String pathToIndexTxt = accountUtils.pathToAccountsFolder() + "index.txt";
+    Map<String, String> readingIndexAccount = dataDirectory.readFileHashmap(pathToIndexTxt);
 
+    for (Map.Entry<String, String> entry : readingIndexAccount.entrySet()) {
+      System.out.println(entry.getKey() + " (" + entry.getValue() + ")");
+    }
+  }
 }
---
+++ src/test/java/com/hedera/cli/hedera/crypto/AccountListTest.java
@@ -0,0 +1,38 @@
+package com.hedera.cli.hedera.crypto;
+
+import com.hedera.cli.hedera.utils.AccountUtils;
+import com.hedera.cli.hedera.utils.DataDirectory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountListTest {
+
+    @Test
+    public void testStreamList() {
+        AccountUtils accountUtils = new AccountUtils();
+        String pathToIndexTxt = accountUtils.pathToAccountsFolder() + "index.txt";
+
+        HashMap<String, String> testMap = new HashMap<>();
+        testMap.put("0.0.90304", "aggressive_primerose_3092");
+        testMap.put("0.0.82319", "gloomy_alyssum_270");
+        testMap.put("0.0.1003", "wiry_bryn_3883");
+        testMap.put("0.0.1009", "jaunty_mint_465");
+        testMap.put("0.0.112232", "definitive_forsythia_2853");
+        testMap.put("0.0.8888", "sorrowful_geranium_7578");
+
+        DataDirectory dataDirectory = Mockito.mock(DataDirectory.class);
+        when(dataDirectory.readFileHashmap(pathToIndexTxt)).thenReturn(testMap);
+        Map<String, String> readingIndexAccount = dataDirectory.readFileHashmap(pathToIndexTxt);
+        assertEquals(readingIndexAccount.entrySet(), testMap.entrySet());
+    }
+}
