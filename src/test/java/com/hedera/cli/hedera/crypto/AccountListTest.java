52bf8bbfa0ec30477ca371d38c3ad0707fdcba91
Calvin Cheng <linchuan.cheng@gmail.com>
2019-10-18 07:09:49 +0800

    Fix AccountListTest
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- src/test/java/com/hedera/cli/hedera/crypto/AccountListTest.java
+++ src/test/java/com/hedera/cli/hedera/crypto/AccountListTest.java
@@ -1,26 +1,50 @@
 package com.hedera.cli.hedera.crypto;
 
-import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.hedera.cli.hedera.utils.AccountUtils;
+import com.hedera.cli.hedera.utils.DataDirectory;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AccountListTest {
+
+    @InjectMocks
+    AccountUtils accountUtils;
+
+    @Mock
+    DataDirectory dataDirectory;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testStreamList() {
-        System.out.println("Temporarily disabled until we review our test infrastructure");
-        // AccountUtils accountUtils = new AccountUtils();
-        // String pathToIndexTxt = accountUtils.pathToAccountsFolder() + "index.txt";
+        String pathToIndexTxt = accountUtils.pathToIndexTxt();
 
-        // HashMap<String, String> testMap = new HashMap<>();
-        // testMap.put("0.0.90304", "aggressive_primerose_3092");
-        // testMap.put("0.0.82319", "gloomy_alyssum_270");
-        // testMap.put("0.0.1003", "wiry_bryn_3883");
-        // testMap.put("0.0.1009", "jaunty_mint_465");
-        // testMap.put("0.0.112232", "definitive_forsythia_2853");
-        // testMap.put("0.0.8888", "sorrowful_geranium_7578");
+        HashMap<String, String> testMap = new HashMap<>();
+        testMap.put("0.0.90304", "aggressive_primerose_3092");
+        testMap.put("0.0.82319", "gloomy_alyssum_270");
+        testMap.put("0.0.1003", "wiry_bryn_3883");
+        testMap.put("0.0.1009", "jaunty_mint_465");
+        testMap.put("0.0.112232", "definitive_forsythia_2853");
+        testMap.put("0.0.8888", "sorrowful_geranium_7578");
 
-        // DataDirectory dataDirectory = Mockito.mock(DataDirectory.class);
-        // when(dataDirectory.readFileHashmap(pathToIndexTxt)).thenReturn(testMap);
-        // Map<String, String> readingIndexAccount = dataDirectory.readFileHashmap(pathToIndexTxt);
-        // assertEquals(readingIndexAccount.entrySet(), testMap.entrySet());
+        when(dataDirectory.readIndexToHashmap(pathToIndexTxt)).thenReturn(testMap);
+        Map<String, String> readingIndexAccount = dataDirectory.readIndexToHashmap(pathToIndexTxt);
+        assertTrue(readingIndexAccount.entrySet().equals(testMap.entrySet()));
     }
 }
