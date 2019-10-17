c057baf33361876983578da261c3b620f6b6c0c5
Calvin Cheng <linchuan.cheng@gmail.com>
2019-10-17 12:04:51 +0800

    Ditto for file subcommands. They should be able to handle options with spaces
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- src/main/java/com/hedera/cli/HederaFile.java
+++ src/main/java/com/hedera/cli/HederaFile.java
@@ -1,5 +1,8 @@
 package com.hedera.cli;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.hedera.cli.config.InputReader;
 import com.hedera.cli.defaults.CliDefaults;
 import com.hedera.cli.hedera.file.File;
@@ -31,7 +34,35 @@ public class HederaFile extends CliDefaults {
     @ShellMethod(value = "manage hedera file")
     public void file(
             @ShellOption(defaultValue = "") String subCommand,
-            @ShellOption(defaultValue = "", arity = -1) String... args) {
+            // file create
+            @ShellOption(value = { "-c", "--contentString" }, defaultValue = "") String c,
+			@ShellOption(value = { "-d", "--date" }, defaultValue = "") String d,
+            @ShellOption(value = { "-s", "--fileSizeByte" }, defaultValue = "") String s,
+            @ShellOption(value = { "-t", "--maxTransactionFee" }, defaultValue = "") String t,
+            // file delete
+            @ShellOption(value = {"-f", "--fileID"}, defaultValue = "") String f) {
+    
+        String[] args = new String[]{};
+
+        // @formatter:off
+        if (subCommand.equals("create")) {
+			ArrayList<String> argsList = new ArrayList<String>();
+			if (!c.isEmpty()) argsList.add(c);
+            if (!d.isEmpty()) argsList.add(d);
+            if (!s.isEmpty()) argsList.add(s);
+            if (!t.isEmpty()) argsList.add(t);
+			Object[] objs = argsList.toArray();
+			args = Arrays.copyOf(objs, objs.length, String[].class);
+        }
+
+        if (subCommand.equals("delete")) {
+            ArrayList<String> argsList = new ArrayList<String>();
+            if (!f.isEmpty()) argsList.add(f);
+            Object[] objs = argsList.toArray();
+			args = Arrays.copyOf(objs, objs.length, String[].class);
+        }
+        // @formatter:on
+        
         File file = new File();
         file.handle(context, inputReader, subCommand, args);
     }
