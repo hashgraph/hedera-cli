5dde5a97020dc7176ba5c5fb3b0f33d6cad61e43
Calvin Cheng <linchuan.cheng@gmail.com>
2019-10-18 07:02:06 +0800

    AccountListTest with InjectMocks and Mock
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- src/test/java/com/hedera/cli/hedera/crypto/AccountListTest.java
+++ src/test/java/com/hedera/cli/hedera/crypto/AccountListTest.java
@@ -1,6 +1,6 @@
 package com.hedera.cli.hedera.crypto;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
@@ -9,14 +9,30 @@ import java.util.Map;
 import com.hedera.cli.hedera.utils.AccountUtils;
 import com.hedera.cli.hedera.utils.DataDirectory;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class AccountListTest {
+
+    @InjectMocks
+    AccountUtils accountUtils;
+
+    @Mock
+    DataDirectory dataDirectory;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testStreamList() {
-        AccountUtils accountUtils = new AccountUtils();
         String pathToIndexTxt = accountUtils.pathToIndexTxt();
 
         HashMap<String, String> testMap = new HashMap<>();
@@ -27,9 +43,8 @@ public class AccountListTest {
         testMap.put("0.0.112232", "definitive_forsythia_2853");
         testMap.put("0.0.8888", "sorrowful_geranium_7578");
 
-        DataDirectory dataDirectory = Mockito.mock(DataDirectory.class);
         when(dataDirectory.readIndexToHashmap(pathToIndexTxt)).thenReturn(testMap);
         Map<String, String> readingIndexAccount = dataDirectory.readIndexToHashmap(pathToIndexTxt);
-        assertEquals(readingIndexAccount.entrySet(), testMap.entrySet());
+        assertTrue(readingIndexAccount.entrySet().equals(testMap.entrySet()));
     }
 }
