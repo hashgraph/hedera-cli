d4ceaaaff42678a5ea13cd14e2561794eef3ee85
Calvin Cheng <calvin@hedera.com>
2019-10-21 16:11:21 +0800

    Placeholder before we begin increasing test coverage
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- src/test/java/com/hedera/cli/models/NetworkTest.java
+++ src/test/java/com/hedera/cli/models/NetworkTest.java
@@ -1,5 +1,7 @@
 package com.hedera.cli.models;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class NetworkTest {
@@ -9,18 +11,21 @@ public class NetworkTest {
 
   @Test
   public void getNetworks() {
+
+    assertTrue(true);
     // List<Network> networks = addressBook.getNetworks();
     // int expected = 3;
     // int actual = networks.size();
     // assertEquals(expected, actual);
-  
+
     // Network network = networks.get(0);
     // assertEquals("mainnet", network.getName());
 
     // List<HederaNode> mainnetNodes = network.getNodes();
     // assertEquals(10, mainnetNodes.size());
 
-    // // retrieve a random node and check that we can get it back through its account id
+    // // retrieve a random node and check that we can get it back through its
+    // account id
     // HederaNode node = network.getRandomNode();
     // HederaNode nodeActual = network.getNodeByAccountId(node.getAccount());
     // assertEquals(node, nodeActual);
