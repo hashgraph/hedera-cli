2224112d40c127f5b962072d85269e454316ab02
Calvin Cheng <calvin@hedera.com>
2019-10-21 15:57:54 +0800

    This is a bug that was caught by lint
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- src/main/java/com/hedera/cli/hedera/crypto/AccountUpdate.java
+++ src/main/java/com/hedera/cli/hedera/crypto/AccountUpdate.java
@@ -3,6 +3,7 @@ package com.hedera.cli.hedera.crypto;
 import com.hedera.cli.hedera.Hedera;
 import com.hedera.cli.shell.ShellHelper;
 import com.hedera.hashgraph.sdk.HederaException;
+import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.account.AccountUpdateTransaction;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
 
@@ -38,24 +39,25 @@ public class AccountUpdate implements Runnable {
 
             // First, we create a new account so we don't affect our account
             var originalKey = Ed25519PrivateKey.generate();
-            var accountId = client.createAccount(originalKey.getPublicKey(), 0);
+            var newAccountId = client.createAccount(originalKey.getPublicKey(), 0);
 
             // Next, we update the key
             var newKey = Ed25519PrivateKey.generate();
-            System.out.println(" :: update public key of account " + accountId);
-            System.out.println("set key = " + newKey.getPublicKey());
-            new AccountUpdateTransaction(client).setAccountForUpdate(accountId)
+            shellHelper.printInfo(" :: update public key of account " + accountId);
+            shellHelper.printInfo("set key = " + newKey.getPublicKey());
+            var oldAccountId = AccountId.fromString(accountId);
+            new AccountUpdateTransaction(client).setAccountForUpdate(oldAccountId)
                     .setKey(newKey.getPublicKey())
                     // Sign with the previous key and the new key
                     .sign(originalKey)
                     .sign(newKey)
                     .executeForReceipt();
             // Now we fetch the account information to check if the key was changed
-            System.out.println(" :: getAccount and check our current key");
+            shellHelper.printInfo(" :: getAccount and check our current key");
 
-            var info = client.getAccount(accountId);
+            var info = client.getAccount(newAccountId);
 
-            System.out.println("key = " + info.getKey());
+            shellHelper.printInfo("key = " + info.getKey());
         } catch (HederaException e) {
             shellHelper.printError(e.getMessage());
         }
