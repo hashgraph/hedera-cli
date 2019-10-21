7befa5ccba7f2c12b86424dc15b45c470016fdf9
Calvin Cheng <calvin@hedera.com>
2019-10-21 11:50:50 +0800

    use mock directly
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- src/test/java/com/hedera/cli/defaults/CliDefaultsTest.java
+++ src/test/java/com/hedera/cli/defaults/CliDefaultsTest.java
@@ -1,6 +1,7 @@
 package com.hedera.cli.defaults;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.hedera.cli.hedera.utils.DataDirectory;
@@ -10,7 +11,6 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.shell.Availability;
@@ -27,7 +27,7 @@ public class CliDefaultsTest {
     @Test
     public void testIsDefaultNetworkAndAccountSet() {
 
-        DataDirectory dataDirectory = Mockito.mock(DataDirectory.class);
+        DataDirectory dataDirectory = mock(DataDirectory.class);
         when(dataDirectory.readFile("network.txt", "testnet")).thenReturn("testnet");
 
         CliDefaults defaults = new CliDefaults() {
