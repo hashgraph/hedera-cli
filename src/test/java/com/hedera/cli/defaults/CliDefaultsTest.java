74df130849cd48c25e8700e6a4f303a1e2089203
Calvin Cheng <calvin@hedera.com>
2019-10-23 14:20:32 +0800

    100% test coverage for defaults package
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- src/test/java/com/hedera/cli/defaults/CliDefaultsTest.java
+++ src/test/java/com/hedera/cli/defaults/CliDefaultsTest.java
@@ -10,29 +10,70 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
 
 @ExtendWith(MockitoExtension.class)
 public class CliDefaultsTest {
 
-    // captures our stdout
-    // @Rule
-    // public OutputCaptureRule capture = new OutputCaptureRule();
+    // Example Cli Test Class that extends CliDefaults abstract class
+    private static class ExampleCli extends CliDefaults {
+        @ShellMethodAvailability("isNotCompleted")
+        @ShellMethod(value = "example command")
+        public void exampleCommmand() {
+            System.out.println("Example stub function");
+        }
+    }
 
     @Test
-    public void testIsDefaultNetworkAndAccountSet() {
-
+    public void bothDefaultNetworkAndDefaultAccountAreSet() {
         DataDirectory dataDirectory = mock(DataDirectory.class);
+        // default network is set as "testnet"
         when(dataDirectory.readFile("network.txt", "testnet")).thenReturn("testnet");
+        // default account is set as "0.0.1234"
+        when(dataDirectory.readFile("testnet/accounts/default.txt")).thenReturn("0.0.1234");
 
-        CliDefaults defaults = new CliDefaults() {
-            @Override
-            public Availability isDefaultNetworkAndAccountSet() {
-                return super.isDefaultNetworkAndAccountSet();
-            }
-        };
-        defaults.setDataDirectory(dataDirectory);
-        Availability availability = defaults.isDefaultNetworkAndAccountSet();
+        ExampleCli exampleCli = new ExampleCli();
+        exampleCli.setDataDirectory(dataDirectory);
+
+        Availability availability = exampleCli.isDefaultNetworkAndAccountSet();
         assertEquals(Availability.available().getReason(), availability.getReason());
     }
-}
 
+    @Test
+    public void defaultNetworkNotSet() {
+        DataDirectory dataDirectory = mock(DataDirectory.class);
+        // this should never happen, since network.txt will return "testnet" if "network.txt" is emptys
+        // but we emulate the scenario when it is indeed empty
+        // default network is not set for some reason
+        when(dataDirectory.readFile("network.txt", "testnet")).thenReturn("");
+
+        ExampleCli exampleCli = new ExampleCli();
+        exampleCli.setDataDirectory(dataDirectory);
+
+        Availability availability = exampleCli.isDefaultNetworkAndAccountSet();
+        assertEquals("you have not set your default network", availability.getReason());
+    }
+
+    @Test
+    public void defaultAccountNotSet() {
+        DataDirectory dataDirectory = mock(DataDirectory.class);
+        // default network is set
+        when(dataDirectory.readFile("network.txt", "testnet")).thenReturn("testnet");
+        // default account is NOT set
+        when(dataDirectory.readFile("testnet/accounts/default.txt")).thenReturn("");
+        
+        ExampleCli exampleCli = new ExampleCli();
+        exampleCli.setDataDirectory(dataDirectory);
+
+        Availability availability = exampleCli.isDefaultNetworkAndAccountSet();
+        assertEquals("you have not set your default account for the current network", availability.getReason());
+    }
+
+    @Test
+    public void isNotCompleted() {
+        ExampleCli exampleCli = new ExampleCli();
+        assertEquals("it is not completed", exampleCli.isNotCompleted().getReason());
+    }
+
+}
