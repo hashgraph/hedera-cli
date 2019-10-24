6bcd7653118bed70990dc5dabac705b63658077a
Calvin Cheng <calvin@hedera.com>
2019-10-24 18:41:54 +0800

    Test coverage for models package
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

---
+++ src/test/java/com/hedera/cli/models/RecoveredAccountModelTest.java
@@ -0,0 +1,33 @@
+package com.hedera.cli.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RecoveredAccountModelTest {
+
+  @Test
+  void recoveredAccountModel() {
+
+    RecoveredAccountModel r = new RecoveredAccountModel();
+
+    r.setAccountId("0.0.1001");
+    r.setPrivateKey("somePrivateKey");
+    r.setPublicKey("somePublicKey");
+    r.setPrivateKeyEncoded("somePrivateKeyInASN1");
+    r.setPublicKeyEncoded("somePublicKeyInASN1");
+    r.setPrivateKeyBrowserCompatible("someBrowserCompatiblePrivateKey");
+
+
+    assertEquals("0.0.1001", r.getAccountId());
+    assertEquals("somePrivateKey", r.getPrivateKey());
+    assertEquals("somePublicKey", r.getPublicKey());
+    assertEquals("somePrivateKeyInASN1", r.getPrivateKeyEncoded());
+    assertEquals("somePublicKeyInASN1", r.getPublicKeyEncoded());
+    assertEquals("someBrowserCompatiblePrivateKey", r.getPrivateKeyBrowserCompatible());
+  }
+
+}
