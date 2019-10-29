c1034bd57fc7690dc235ee4ba8e1528fd2adbf97
Serene Lim <serene@hedera.com>
2019-10-29 13:45:09 +0800

    Codacy fixes remove unused imports
    Signed-off-by: Serene Lim <serene@hedera.com>

--- src/test/java/com/hedera/cli/hedera/crypto/AccountBalanceTest.java
+++ src/test/java/com/hedera/cli/hedera/crypto/AccountBalanceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
