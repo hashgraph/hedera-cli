ae3f21afdaebeaf291ded4163b79bf11f73c859d
Calvin Cheng <calvin@hedera.com>
2019-10-29 23:09:01 +0800

    Update bip39 tests
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- src/test/java/com/hedera/cli/hedera/bip39/MnemonicTest.java
+++ src/test/java/com/hedera/cli/hedera/bip39/MnemonicTest.java
@@ -1,33 +1,43 @@
 package com.hedera.cli.hedera.bip39;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.hedera.cli.hedera.bip39.MnemonicException.MnemonicLengthException;
+import com.hedera.cli.hedera.bip39.MnemonicException.MnemonicWordException;
 
 import org.junit.jupiter.api.Test;
 
 public class MnemonicTest {
 
+  @SuppressWarnings("serial")
   @Test
   public void testToEntropy() {
-    // to be completed
-    assertTrue(true);
-  }
+    List<String> words = Arrays.asList(EnglishHelper.words);
+    List<String> wordsNotMultipleOf3 = words.subList(0, 1);
 
-  @Test
-  public void testToMnemonic() {
-    // to be completed
-    assertTrue(true);
-  }
+    // if we only have two words, an exception will be thrown
+    Mnemonic m = new Mnemonic();
+    assertThrows(MnemonicLengthException.class, () -> {
+      m.toEntropy(wordsNotMultipleOf3);
+    });
 
-  @Test
-  public void testCheck() {
-    // to be completed
-    assertTrue(true);
-  }
+    // if we have no words at all, an exception will be thrown
+    assertThrows(MnemonicLengthException.class, () -> {
+      m.toEntropy(new ArrayList<String>());
+    });
 
-  @Test
-  public void testBytesToBits() {
-    // to be completed
-    assertTrue(true);
+    // if we have words that is not in EnglishHelper.words at all, an exception will be thrown
+    assertThrows(MnemonicWordException.class, () -> {
+      m.toEntropy(new ArrayList<String>() {{
+        add("A");
+        add("B");
+        add("C");
+      }});
+    });
   }
 
 }
