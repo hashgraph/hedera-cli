22cd654e9f5c964ab8ca3ffc362835a842502b33
Serene Lim <serene@hedera.com>
2019-10-29 13:43:36 +0800

    Codacy fixes
    Signed-off-by: Serene Lim <serene@hedera.com>

--- src/test/java/com/hedera/cli/hedera/crypto/AccountBalanceTest.java
+++ src/test/java/com/hedera/cli/hedera/crypto/AccountBalanceTest.java
@@ -3,12 +3,11 @@ package com.hedera.cli.hedera.crypto;
 import com.hedera.cli.hedera.Hedera;
 import com.hedera.cli.hedera.utils.AccountUtils;
 import com.hedera.cli.shell.ShellHelper;
-import com.hedera.hashgraph.sdk.Client;
-import com.hedera.hashgraph.sdk.HederaException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,7 +15,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,24 +31,6 @@ public class AccountBalanceTest {
 
     @Mock
     private Hedera hedera;
-
-//    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-
-    @Test
-    public void getBalance() throws HederaException {
-//        String accountId = "0.0.1001";
-        Client client = mock(Client.class);
-        System.out.println(client);
-//        when(client.getAccountBalance(AccountId.fromString(accountId))).thenReturn(100L);
-//        doNothing().when(shellHelper).printError(null);
-        long value = accountBalance.getBalance();
-//        verify(client, times(1)).getAccountBalance(AccountId.fromString(accountId));
-        assertEquals(0, value);
-//        String outputResult = new String(output.toByteArray());
-//        List<String> outputResultArray = Arrays.asList(outputResult.split("\n"));
-//        outputResultArray.stream().map(s -> s.trim()).collect(Collectors.toList());
-//        assertThat(outputResultArray, containsInAnyOrder("Balance "));
-    }
 
     @Test
     public void autoWiredDependenciesNotNull() {
