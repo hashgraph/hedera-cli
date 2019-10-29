e9ac392dac733f6beab3fa8d2dcf4058de9951fd
Calvin Cheng <calvin@hedera.com>
2019-10-29 17:45:29 +0800

    Lint fixes
    Signed-off-by: Calvin Cheng <calvin@hedera.com>

--- src/test/java/com/hedera/cli/hedera/crypto/AccountBalanceTest.java
+++ src/test/java/com/hedera/cli/hedera/crypto/AccountBalanceTest.java
@@ -1,20 +1,19 @@
 package com.hedera.cli.hedera.crypto;
 
-import com.hedera.cli.hedera.Hedera;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.hedera.cli.hedera.utils.AccountManager;
 import com.hedera.cli.shell.ShellHelper;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class AccountBalanceTest {
@@ -28,9 +27,6 @@ public class AccountBalanceTest {
     @Mock
     private AccountManager accountManager;
 
-    @Mock
-    private Hedera hedera;
-
     @Test
     public void autoWiredDependenciesNotNull() {
         accountManager = accountBalance.getAccountManager();
@@ -38,9 +34,6 @@ public class AccountBalanceTest {
 
         shellHelper = accountBalance.getShellHelper();
         assertNotNull(shellHelper);
-
-        hedera = accountBalance.getHedera();
-        assertNotNull(hedera);
 
         accountBalance.setAccountIdInString("0.0.1111");
         String accountIdInString = accountBalance.getAccountIdInString();
--- src/test/java/com/hedera/cli/hedera/setup/SetupTest.java
+++ src/test/java/com/hedera/cli/hedera/setup/SetupTest.java
@@ -1,14 +1,11 @@
 package com.hedera.cli.hedera.setup;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
-import com.hedera.cli.hedera.Hedera;
-import com.hedera.cli.hedera.crypto.AccountRecovery;
 import com.hedera.cli.hedera.keygen.EDBip32KeyChain;
 import com.hedera.cli.hedera.keygen.KeyGeneration;
 import com.hedera.cli.hedera.keygen.KeyPair;
@@ -37,12 +34,6 @@ public class SetupTest {
 
     @Mock
     private ShellHelper shellHelper;
-
-    @Mock
-    private AccountRecovery accountRecovery;
-
-    @Mock
-    private Hedera hedera;
 
     // not a mock
     private String accountId;
@@ -77,76 +68,4 @@ public class SetupTest {
         assertEquals(keyPair.getPublicKeyEncodedHex(), recoveredAccountModel.getPublicKeyEncoded());
         assertEquals(keyPair.getSeedAndPublicKeyHex(), recoveredAccountModel.getPrivateKeyBrowserCompatible());
     }
-
-    @Test
-    public void autoWiredDependenciesNotNull() {
-        hedera = setup.getHedera();
-        assertNotNull(hedera);
-
-        accountRecovery = setup.getAccountRecovery();
-        assertNotNull(accountRecovery);
-    }
-
-//    @Test
-//    public void handleSetupWithBipRecoveryWords() {
-//        prepareTestData();
-//
-//        String accountId = "0.0.5432";
-//        Client client = new Client(AccountId.fromString("0.0.3"), "35.188.20.11:50211");
-//        when(inputReader.prompt("Have you migrated your account on Hedera wallet? If migrated, enter `bip`, else enter `hgc`")).thenReturn("bip");
-//        when(inputReader.prompt("account ID in the format of 0.0.xxxx that will be used as default operator")).thenReturn(accountId);
-//        when(inputReader.prompt("24 words phrase", "secret", false)).thenReturn(phrase);
-//        when(accountManager.isAccountId(accountId)).thenReturn(true);
-//        when(accountRecovery.recoverEDKeypairPostBipMigration(Arrays.asList(phrase.split(" ")))).thenReturn(keyPair);
-//
-//        q = new AccountInfoQuery(client)
-//                .setAccountId(AccountId.fromString(accountId))
-//                .setPayment(
-//                        new CryptoTransferTransaction(null)
-//                                .setTransactionId(new TransactionId(new AccountId(2), Instant.now()))
-//                                .setNodeAccountId(new AccountId(3))
-//                                .addSender(new AccountId(2), 10000)
-//                                .addRecipient(new AccountId(3), 10000)
-//                                .setTransactionFee(100_000)
-//                                .sign(Ed25519PrivateKey.fromString(keyPair.getPrivateKeyHex())))
-//                .setAccountId(AccountId.fromString(accountId));
-//
-//        assertThrows(HederaException.class, () -> {
-//            when(q.execute()).thenCallRealMethod();
-//        });
-//
-//        setup.handle(inputReader, shellHelper);
-//        cleanUpTestData();
-//    }
-//
-//    @Test
-//    public void handleSetupWithHgcRecoveryWords() {
-//        prepareTestData();
-//
-//        String accountId = "0.0.9876";
-//        Client client = new Client(AccountId.fromString("0.0.3"), "35.188.20.11:50211");
-//        when(inputReader.prompt("Have you migrated your account on Hedera wallet? If migrated, enter `bip`, else enter `hgc`")).thenReturn("hgc");
-//        when(inputReader.prompt("account ID in the format of 0.0.xxxx that will be used as default operator")).thenReturn(accountId);
-//        when(inputReader.prompt("24 words phrase", "secret", false)).thenReturn(phrase);
-//        when(accountManager.isAccountId(accountId)).thenReturn(true);
-//        when(accountRecovery.recoverEd25519AccountKeypair(Arrays.asList(phrase.split(" ")), accountId, shellHelper)).thenReturn(keyPair);
-//
-//        q = new AccountInfoQuery(client)
-//                .setAccountId(AccountId.fromString(accountId))
-//                .setPayment(
-//                        new CryptoTransferTransaction(null)
-//                                .setTransactionId(new TransactionId(new AccountId(2), Instant.now()))
-//                                .setNodeAccountId(new AccountId(3))
-//                                .addSender(new AccountId(2), 10000)
-//                                .addRecipient(new AccountId(3), 10000)
-//                                .setTransactionFee(100_000)
-//                                .sign(Ed25519PrivateKey.fromString(keyPair.getPrivateKeyHex())))
-//                .setAccountId(AccountId.fromString(accountId));
-//
-//        assertThrows(HederaException.class, () -> {
-//            when(q.execute()).thenCallRealMethod();
-//        });
-//        setup.handle(inputReader, shellHelper);
-//        cleanUpTestData();
-//    }
 }
