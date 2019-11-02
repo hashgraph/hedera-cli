6b98f49fe0c91d40f6a88e87eb31e6ad8d2977bf
Calvin Cheng <linchuan.cheng@gmail.com>
2019-11-03 04:45:23 +0800

    CliPromptProviderTest
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- src/main/java/com/hedera/cli/shell/CliPromptProvider.java
+++ src/main/java/com/hedera/cli/shell/CliPromptProvider.java
@@ -1,33 +1,24 @@
 package com.hedera.cli.shell;
 
 import com.hedera.cli.hedera.Hedera;
-import com.hedera.cli.services.CurrentAccountService;
 
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStringBuilder;
 import org.jline.utils.AttributedStyle;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.stereotype.Component;
-
-import io.grpc.netty.shaded.io.netty.util.internal.StringUtil;
 
 @Component
 public class CliPromptProvider implements PromptProvider {
 
     @Autowired
-    private ApplicationContext context;
-
-    @Autowired
     private Hedera hedera;
-
-    private AttributedString currentAccountAttr;
 
     @Override
     public AttributedString getPrompt() {
-        String currentNetwork = hedera.addressBookManager.getCurrentNetworkAsString();
-        String defaultAccount = hedera.getDefaultAccount();
+        String currentNetwork = hedera.getAddressBookManager().getCurrentNetworkAsString();
+        String operatorAccount = hedera.getOperatorAccount();
 
         // red
         AttributedString noDefaultAccountAttr = new AttributedString(
@@ -51,24 +42,24 @@ public class CliPromptProvider implements PromptProvider {
         // default operator account for this network has been set or not
         AttributedStringBuilder builder = new AttributedStringBuilder();
 
-        if (defaultAccount.isEmpty()) {
-            return builder.append(noDefaultAccountAttr).append(hederaAttr).append(currentNetworkAttr).append(promptAttr)
-                    .toAttributedString();
-        }
-
-        // yellow
-        String currAccount = defaultAccount.split(":")[1];
-        CurrentAccountService currentAccountService = context.getBean("currentAccount", CurrentAccountService.class);
-        String accountNumber = currentAccountService.getAccountNumber();
-        if (!StringUtil.isNullOrEmpty(accountNumber)) {
-            currentAccountAttr = new AttributedString("[" + accountNumber + "]",
-                    AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
-        } else {
-            currentAccountAttr = new AttributedString("[" + currAccount + "]",
-                    AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
-        }
-
-        return builder.append(hederaAttr).append(currentNetworkAttr).append(currentAccountAttr).append(promptAttr)
+        if (operatorAccount.isEmpty()) { // @formatter:off
+            return builder
+                .append(noDefaultAccountAttr)
+                .append(hederaAttr)
+                .append(currentNetworkAttr)
+                .append(promptAttr)
                 .toAttributedString();
-    }
+        } 
+        
+        // yellow
+        AttributedString currentAccountAttr = new AttributedString("[" + operatorAccount + "]",
+                AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
+
+        return builder
+                .append(hederaAttr)
+                .append(currentNetworkAttr)
+                .append(currentAccountAttr)
+                .append(promptAttr)
+                .toAttributedString();
+    } // @formatter:on
 }
