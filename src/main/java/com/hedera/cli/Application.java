4bdb3e151ad9fb9a336a76377323a344e690c532
Calvin Cheng <linchuan.cheng@gmail.com>
2020-01-07 12:48:13 +0800

    Spring Banner.Mode.OFF when we execute our cli in non-interactive mode

--- src/main/java/com/hedera/cli/Application.java
+++ src/main/java/com/hedera/cli/Application.java
@@ -2,6 +2,7 @@ package com.hedera.cli;
 
 import com.hedera.cli.services.ExecutionService;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,16 +15,21 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
     public static void main(String[] args) {
         // by default, cli executes in interactive mode (mode = true)
+        boolean bannerMode = true;
         ExecutionService.putCache("X", "true");
         for (String arg: args) {
             // if user specifies -X, we will set cli execution to non-interactive mode (mode = false)
             if (arg.equals("-X")) {
                 ExecutionService.putCache("X", "false");
+                bannerMode = false;
             }
         }
  
         SpringApplicationBuilder appBuilder = new SpringApplicationBuilder(Application.class);
         SpringApplication app = appBuilder.build();
+        if (!bannerMode) {
+            app.setBannerMode(Banner.Mode.OFF);
+        }
         app.run(args);
     }
 }
