d24f061d3a8f834ef76991b7aba490dd8f339292
Serene Lim <serene@hedera.com>
2020-01-21 17:16:46 +0800

    Make sure default operator is set back to original env test account id
    Signed-off-by: Serene Lim <serene@hedera.com>

--- blackbox/account_default.sh
+++ blackbox/account_default.sh
@@ -3,6 +3,10 @@
 expect <<EOF
 set timeout -1
 spawn $EXEC_JAR -X account default $TEST_ACCOUNT_ID_DEFAULT
-
 expect "Default operator updated true"
+
+# set the default operator back to the test accountId
+spawn $EXEC_JAR -X account default $TEST_ACCOUNT_ID
+expect "Default operator updated true"
+
 EOF
---
+++ blackbox/account_recovery.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/expect
+
+set timeout -1
+
+# recover the setup account that was saved in the default when first created
+# provide the values in .env
