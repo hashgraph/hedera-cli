f8a19cfff2d6501012a18affa5667dc2e9b81fd8
Michiel Mulders <michielmulders1@gmail.com>
2023-10-17 12:16:59 +0200

    Add verbose quite logging levels
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

--- src/hedera-cli.ts
+++ src/hedera-cli.ts
@@ -1,12 +1,23 @@
 import { program } from 'commander';
 
 import commands from './commands';
+import { Logger } from './utils/logger';
+
+const logger = Logger.getInstance();
 
 program
   .version('1.0.0')
-  .description('A CLI tool for managing Hedera environments');
+  .description('A CLI tool for managing Hedera environments')
+  .option('-v, --verbose', 'output extra debugging')
+  .option('-q, --quiet', 'output only errors and warnings');
 
-// Commands
+  if (process.argv.includes('-v')) {
+    logger.setLevel('verbose');
+  } else if (process.argv.includes('-q')) {
+    logger.setLevel('quiet');
+  }
+
+  // Commands
 commands.setupCommands(program);
 commands.networkCommands(program);
 commands.accountCommands(program);
