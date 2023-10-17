1720bc4b2443feecb67d6f6e31c9dd87274e74c1
Michiel Mulders <michielmulders1@gmail.com>
2023-10-17 11:40:14 +0200

    Update token interface
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

--- types/api/token.d.ts
+++ types/api/token.d.ts
@@ -1,11 +1,5 @@
 import type { Key } from "./shared";
 
-interface CustomFees {
-  created_timestamp: string;
-  fixed_fees: any[];
-  fractional_fees: any[];
-}
-
 export type TokenResponse = {
   admin_key: Key;
   auto_renew_account: string;
@@ -15,7 +9,7 @@ export type TokenResponse = {
   decimals: string;
   deleted: boolean;
   expiry_timestamp: number;
-  fee_schedule_key: Key; // If there are other possible values, you might want to adjust this type
+  fee_schedule_key: Key;
   freeze_default: boolean;
   freeze_key: Key;
   initial_supply: string;
@@ -27,11 +21,37 @@ export type TokenResponse = {
   pause_key: Key;
   pause_status: string;
   supply_key: Key;
-  supply_type: "INFINITE" | "FINITE"; // Adjust if there are other possible values
+  supply_type: "INFINITE" | "FINITE";
   symbol: string;
   token_id: string;
   total_supply: string;
   treasury_account_id: string;
-  type: string; // possible values? "FUNGIBLE_COMMON"
+  type: string; // possible values? "FUNGIBLE_COMMON" | "NON_FUNGIBLE_UNIQUE" | "ALL"
   wipe_key: Key;
+};
+
+interface CustomFees {
+  created_timestamp: string;
+  fixed_fees: FixedFee[];
+  fractional_fees: FractionalFee[];
+}
+
+type FixedFee = {
+  amount: number;
+  collector_account_id: string;
+  denominating_token_id: string;
+};
+
+type FractionalFee = {
+  amount: Fraction;
+  collector_account_id: string;
+  denominating_token_id: string;
+  maximum: number;
+  minimum: number;
+  net_of_transfers: boolean;
+};
+
+type Fraction = {
+  numerator: number;
+  denominator: number;
 };
