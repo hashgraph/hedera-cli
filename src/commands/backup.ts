aca228e16792b64482f257c64dec354256f7c0bf
Michiel Mulders <michielmulders1@gmail.com>
2023-12-12 14:48:41 +0100

    Add safe option to backup creation removing sensitive info
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

--- src/commands/backup.ts
+++ src/commands/backup.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 
 import { recordCommand } from '../state/stateService';
 
-import type { Command } from '../../types';
+import type { Command, State } from '../../types';
 
 export default (program: any) => {
   const network = program.command('backup');
@@ -15,18 +15,25 @@ export default (program: any) => {
     })
     .description('Create a backup of the config.json file')
     .option('--accounts', 'Backup the accounts')
+    .option('--safe', 'Remove the private keys from the backup')
     .action((options: BackupOptions) => {
-      backupState(options.accounts);
+      backupState(options.accounts, options.safe);
     });
 };
 
-function backupState(backupAccounts: boolean) {
+/**
+ * Create a backup of the state file
+ * 
+ * @param backupAccounts Only backup the accounts from state
+ * @param safe Remove the private keys from the backup file 
+ */
+function backupState(backupAccounts: boolean, safe: boolean) {
   const timestamp = Date.now(); // UNIX timestamp in milliseconds
 
   let data;
   try {
     const statePath = path.join(__dirname, '..', 'state', 'state.json');
-    data = fs.readFileSync(statePath, 'utf8');
+    data = JSON.parse(fs.readFileSync(statePath, 'utf8')) as State;
   } catch (error) {
     console.error('Error reading the state file:', error);
     return;
@@ -34,17 +41,20 @@ function backupState(backupAccounts: boolean) {
 
   // Create backup filename
   let backupFilename = `state.backup.${timestamp}.json`;
-  
+
+  if (safe) {
+    data = filterState(data);
+  }
+
   // Only backup accounts if the user specified the --accounts flag
   if (backupAccounts) {
     backupFilename = `accounts.backup.${timestamp}.json`;
-    data = JSON.stringify(JSON.parse(data).accounts, null, 2);
+    data = data.accounts;
   }
   const backupPath = path.join(__dirname, '..', 'state', backupFilename);
 
-
   try {
-    fs.writeFileSync(backupPath, data, 'utf8');
+    fs.writeFileSync(backupPath, JSON.stringify(data, null, 2), 'utf8');
   } catch (error) {
     console.error('Error creating the backup file:', error);
     return;
@@ -53,6 +63,43 @@ function backupState(backupAccounts: boolean) {
   console.log(`Backup created successfully: ${backupFilename}`);
 }
 
+/**
+ * Remove the private keys and other sensitive info from the state object
+ * Warning: It does not remove the private keys from scripts
+ * 
+ * @param data Modify the state object to remove private keys and other sensitive info
+ * @returns @type {State}
+ */
+function filterState(data: State) {
+  const filteredState = { ...data };
+
+  filteredState.operatorId = '';
+  filteredState.operatorKey = '';
+
+  Object.keys(filteredState.tokens).forEach((key) => {
+    filteredState.tokens[key].keys = {
+      adminKey: '',
+      pauseKey: '',
+      supplyKey: '',
+      wipeKey: '',
+      feeScheduleKey: '',
+      treasuryKey: '',
+      freezeKey: '',
+      kycKey: '',
+    };
+  });
+
+  // Remove the private keys from the accounts
+  Object.keys(filteredState.accounts).forEach((alias) => {
+    filteredState.accounts[alias].privateKey = '';
+  });
+
+  console.log('Warning: The private keys were not removed from scripts');
+
+  return filteredState;
+}
+
 interface BackupOptions {
   accounts: boolean;
+  safe: boolean;
 }
