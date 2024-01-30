73ee6921216e39b00a62e81d643913befebfa80f
Michiel Mulders <michielmulders1@gmail.com>
2024-01-30 16:54:54 +0100

    Add console logging for clearing state script
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

--- clear-state.js
+++ clear-state.js
@@ -5,6 +5,8 @@ async function main() {
 
     const baseState = await fs.readFileSync('./src/state/base_state.json', 'utf8');
     fs.writeFileSync('./src/state/state.json', baseState);
+
+    console.log('Finished clearing state/state.json file');
 }
 
 main();
