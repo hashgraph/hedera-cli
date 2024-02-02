d8b444f229ce294457d487605af267713073bfaf
Michiel Mulders <michielmulders1@gmail.com>
2024-02-02 13:50:44 +0100

    Update mock for account create test
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

--- __tests__/commands/account/create.test.ts
+++ __tests__/commands/account/create.test.ts
@@ -3,8 +3,26 @@ import { Command } from "commander";
 import commands from "../../../src/commands";
 import accountUtils from "../../../src/utils/account";
 import stateController from "../../../src/state/stateController";
+import { AccountId } from "@hashgraph/sdk";
 
 jest.mock("../../../src/state/state"); // Mock the original module -> looks for __mocks__/state.ts in same directory
+jest.mock('@hashgraph/sdk', () => {
+  const originalModule = jest.requireActual('@hashgraph/sdk');
+
+  return {
+    ...originalModule,
+    AccountCreateTransaction: jest.fn().mockImplementation(() => ({
+      setKey: jest.fn().mockReturnThis(),
+      setInitialBalance: jest.fn().mockReturnThis(),
+      setMaxAutomaticTokenAssociations: jest.fn().mockReturnThis(),
+      execute: jest.fn().mockResolvedValue({
+        getReceipt: jest.fn().mockResolvedValue({
+          accountId: AccountId.fromString('0.0.1234'),
+        })
+      }),
+    })),
+  };
+});
 
 describe("account create command", () => {
   beforeEach(() => {
