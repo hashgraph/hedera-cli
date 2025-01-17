3d15b0cbb6fa678d7375655c04506cafdabe12dc
Michiel Mulders <michielmulders1@gmail.com>
2025-01-17 14:46:40 +0100

    feat: Only list available networks (#578)
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

--- __tests__/commands/network/list.test.ts
+++ __tests__/commands/network/list.test.ts
@@ -1,10 +1,24 @@
+import { baseState } from '../../helpers/state';
 import { Command } from "commander";
 import commands from "../../../src/commands";
+import stateController from '../../../src/state/stateController';
+
+jest.mock('../../../src/state/state'); // Mock the original module -> looks for __mocks__/state.ts in same directory
 
 describe("network list command", () => {
   const logSpy = jest.spyOn(console, 'log');
 
   describe("network list - success path", () => {
+    beforeEach(() => {
+      const stateCopy = {
+        ...baseState,
+        // Provide a bogus mainnet operator ID and key
+        localnetOperatorKey: 'mykey',
+      };
+  
+      stateController.saveState(stateCopy);
+    });
+
     afterEach(() => {
       // Spy cleanup
       logSpy.mockClear();
@@ -19,7 +33,9 @@ describe("network list command", () => {
         await program.parse(["node", "hedera-cli.ts", "network", "list"]);
   
         // Assert
-        expect(logSpy).toHaveBeenCalledWith(`Available networks: mainnet, testnet, previewnet, localnet`);
+        expect(logSpy).toHaveBeenCalledWith(`Available networks:`);
+        expect(logSpy).toHaveBeenLastCalledWith(`- localnet`);
+        expect(logSpy).toHaveBeenCalledTimes(2);
       });
   });
 });
