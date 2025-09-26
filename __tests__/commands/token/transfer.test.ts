4aa2f0b4484d9249ef8ed257c4a7b569b3e4b075
michalrozekariane <michal.rozek@arianelabs.com>
2025-09-26 11:29:31 +0200

    debug: some unit tests uncommented for debugging purposes
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- __tests__/commands/token/transfer.test.ts
+++ __tests__/commands/token/transfer.test.ts
@@ -1,88 +1,82 @@
-// import { TransactionId } from '@hashgraph/sdk';
-// import { Command } from 'commander';
-// import commands from '../../../src/commands';
-// import { saveState as storeSaveState } from '../../../src/state/store';
-// import { Logger } from '../../../src/utils/logger';
-// import { alice, bob, tokenState } from '../../helpers/state';
+import { TransactionId } from '@hashgraph/sdk';
+import { Command } from 'commander';
+import commands from '../../../src/commands';
+import { saveState as storeSaveState } from '../../../src/state/store';
+import { Logger } from '../../../src/utils/logger';
+import { alice, bob, tokenState } from '../../helpers/state';
 
-describe('12', () => {
-  test('temporary', () => {
-    console.log('test');
-  });
+const logger = Logger.getInstance();
+
+const tokenId = Object.keys(tokenState.tokens)[0];
+const txId = '0.0.14288@1706880903.830877722';
+jest.mock('@hashgraph/sdk', () => {
+  const originalModule = jest.requireActual('@hashgraph/sdk');
+
+  return {
+    ...originalModule,
+    TransferTransaction: jest.fn().mockImplementation(() => ({
+      addTokenTransfer: jest.fn().mockReturnThis(),
+      sign: jest.fn().mockReturnThis(),
+      freezeWith: jest.fn().mockReturnThis(),
+      execute: jest.fn().mockResolvedValue({
+        transactionId: TransactionId.fromString(txId),
+        getReceipt: jest.fn().mockResolvedValue({
+          status: {
+            _code: 22,
+            message: 'Success',
+          },
+        }),
+      }),
+    })),
+  };
 });
 
-// const logger = Logger.getInstance();
+describe('token transfer command', () => {
+  const logSpy = jest.spyOn(logger, 'log');
 
-// const tokenId = Object.keys(tokenState.tokens)[0];
-// const txId = '0.0.14288@1706880903.830877722';
-// jest.mock('@hashgraph/sdk', () => {
-//   const originalModule = jest.requireActual('@hashgraph/sdk');
+  beforeEach(() => {
+    const tokenStateWithAlice = {
+      ...tokenState,
+      accounts: {
+        [alice.name]: alice,
+        [bob.name]: bob,
+      },
+    };
+    storeSaveState(tokenStateWithAlice as any);
+  });
 
-//   return {
-//     ...originalModule,
-//     TransferTransaction: jest.fn().mockImplementation(() => ({
-//       addTokenTransfer: jest.fn().mockReturnThis(),
-//       sign: jest.fn().mockReturnThis(),
-//       freezeWith: jest.fn().mockReturnThis(),
-//       execute: jest.fn().mockResolvedValue({
-//         transactionId: TransactionId.fromString(txId),
-//         getReceipt: jest.fn().mockResolvedValue({
-//           status: {
-//             _code: 22,
-//             message: 'Success',
-//           },
-//         }),
-//       }),
-//     })),
-//   };
-// });
+  afterEach(() => {
+    // Spy cleanup
+    logSpy.mockClear();
+  });
 
-// describe('token transfer command', () => {
-//   const logSpy = jest.spyOn(logger, 'log');
+  describe('token transfer - success path', () => {
+    test('✅ ', async () => {
+      // Arrange
+      const program = new Command();
+      commands.tokenCommands(program);
+      const balance = 10;
 
-//   beforeEach(() => {
-//     const tokenStateWithAlice = {
-//       ...tokenState,
-//       accounts: {
-//         [alice.name]: alice,
-//         [bob.name]: bob,
-//       },
-//     };
-//     storeSaveState(tokenStateWithAlice as any);
-//   });
+      // Act
+      await program.parseAsync([
+        'node',
+        'hedera-cli.ts',
+        'token',
+        'transfer',
+        '-t',
+        tokenId,
+        '--to',
+        bob.name,
+        '--from',
+        alice.name,
+        '-b',
+        balance.toString(),
+      ]);
 
-//   afterEach(() => {
-//     // Spy cleanup
-//     logSpy.mockClear();
-//   });
-
-//   describe('token transfer - success path', () => {
-//     test('✅ ', async () => {
-//       // Arrange
-//       const program = new Command();
-//       commands.tokenCommands(program);
-//       const balance = 10;
-
-//       // Act
-//       await program.parseAsync([
-//         'node',
-//         'hedera-cli.ts',
-//         'token',
-//         'transfer',
-//         '-t',
-//         tokenId,
-//         '--to',
-//         bob.name,
-//         '--from',
-//         alice.name,
-//         '-b',
-//         balance.toString(),
-//       ]);
-
-//       // Assert
-//       expect(logSpy).toHaveBeenCalledWith(
-//         `Transfer successful with tx ID: ${txId}`,
-//       );
-//     });
-//   });
-// });
+      // Assert
+      expect(logSpy).toHaveBeenCalledWith(
+        `Transfer successful with tx ID: ${txId}`,
+      );
+    });
+  });
+});
