f57ceef16b87f79de3d22eeffc79618e7e6b2283
michalrozekariane <michal.rozek@arianelabs.com>
2025-09-26 11:30:23 +0200

    debug: some unit tests uncommented for debugging purposes
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- __tests__/commands/token/associate.test.ts
+++ __tests__/commands/token/associate.test.ts
@@ -1,72 +1,66 @@
-// import { Command } from 'commander';
-// import commands from '../../../src/commands';
-// import {
-//   get as storeGet,
-//   saveState as storeSaveState,
-// } from '../../../src/state/store';
-// import { alice, tokenState } from '../../helpers/state';
+import { Command } from 'commander';
+import commands from '../../../src/commands';
+import {
+  get as storeGet,
+  saveState as storeSaveState,
+} from '../../../src/state/store';
+import { alice, tokenState } from '../../helpers/state';
 
-describe('9', () => {
-  test('temporary', () => {
-    console.log('test');
-  });
+const tokenId = Object.keys(tokenState.tokens)[0];
+jest.mock('@hashgraph/sdk', () => {
+  const originalModule = jest.requireActual('@hashgraph/sdk');
+
+  return {
+    ...originalModule,
+    TokenAssociateTransaction: jest.fn().mockImplementation(() => ({
+      setAccountId: jest.fn().mockReturnThis(),
+      setTokenIds: jest.fn().mockReturnThis(),
+      sign: jest.fn().mockReturnThis(),
+      freezeWith: jest.fn().mockReturnThis(),
+      execute: jest.fn().mockResolvedValue({
+        getReceipt: jest.fn().mockResolvedValue({}),
+      }),
+    })),
+  };
 });
 
-// const tokenId = Object.keys(tokenState.tokens)[0];
-// jest.mock('@hashgraph/sdk', () => {
-//   const originalModule = jest.requireActual('@hashgraph/sdk');
+describe('token associate command', () => {
+  beforeEach(() => {
+    const tokenStateWithAlice = {
+      ...tokenState,
+      accounts: {
+        [alice.name]: alice,
+      },
+    };
+    storeSaveState(tokenStateWithAlice as any);
+  });
 
-//   return {
-//     ...originalModule,
-//     TokenAssociateTransaction: jest.fn().mockImplementation(() => ({
-//       setAccountId: jest.fn().mockReturnThis(),
-//       setTokenIds: jest.fn().mockReturnThis(),
-//       sign: jest.fn().mockReturnThis(),
-//       freezeWith: jest.fn().mockReturnThis(),
-//       execute: jest.fn().mockResolvedValue({
-//         getReceipt: jest.fn().mockResolvedValue({}),
-//       }),
-//     })),
-//   };
-// });
+  describe('token associate - success path', () => {
+    test('✅ ', async () => {
+      // Arrange
+      const program = new Command();
+      commands.tokenCommands(program);
 
-// describe('token associate command', () => {
-//   beforeEach(() => {
-//     const tokenStateWithAlice = {
-//       ...tokenState,
-//       accounts: {
-//         [alice.name]: alice,
-//       },
-//     };
-//     storeSaveState(tokenStateWithAlice as any);
-//   });
+      // Act
+      await program.parseAsync([
+        'node',
+        'hedera-cli.ts',
+        'token',
+        'associate',
+        '-a',
+        alice.accountId,
+        '-t',
+        tokenId,
+      ]);
 
-//   describe('token associate - success path', () => {
-//     test('✅ ', async () => {
-//       // Arrange
-//       const program = new Command();
-//       commands.tokenCommands(program);
-
-//       // Act
-//       await program.parseAsync([
-//         'node',
-//         'hedera-cli.ts',
-//         'token',
-//         'associate',
-//         '-a',
-//         alice.accountId,
-//         '-t',
-//         tokenId,
-//       ]);
-
-//       // Assert
-//       const tokens = storeGet('tokens' as any);
-//       expect(tokens[tokenId].associations).toEqual([
-//         {
-//           name: alice.name,
-//           accountId: alice.accountId,
-//         },
-//       ]);
-//     });
-//   });
-// });
+      // Assert
+      const tokens = storeGet('tokens' as any);
+      expect(tokens[tokenId].associations).toEqual([
+        {
+          name: alice.name,
+          accountId: alice.accountId,
+        },
+      ]);
+    });
+  });
+});
