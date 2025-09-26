90dc56e5fb91ded26f156b4c13036dd6a6597f69
michalrozekariane <michal.rozek@arianelabs.com>
2025-09-26 11:36:40 +0200

    debug: some unit tests uncommented for debugging purposes
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- __tests__/commands/account/view.test.ts
+++ __tests__/commands/account/view.test.ts
@@ -1,72 +1,66 @@
-// import { Command } from 'commander';
-// import api from '../../../src/api/';
-// import commands from '../../../src/commands';
-// import { saveState as storeSaveState } from '../../../src/state/store';
-// import { Logger } from '../../../src/utils/logger';
-// import { accountResponse } from '../../helpers/api/apiAccountHelper';
-// import { baseState } from '../../helpers/state';
+import { Command } from 'commander';
+import api from '../../../src/api/';
+import commands from '../../../src/commands';
+import { saveState as storeSaveState } from '../../../src/state/store';
+import { Logger } from '../../../src/utils/logger';
+import { accountResponse } from '../../helpers/api/apiAccountHelper';
+import { baseState } from '../../helpers/state';
 
-describe('24', () => {
-  test('temporary', () => {
-    console.log('test');
+const logger = Logger.getInstance();
+
+describe('account view command', () => {
+  const getAccountInfoSpy = jest
+    .spyOn(api.account, 'getAccountInfo')
+    .mockResolvedValue({
+      data: accountResponse,
+    });
+  const logSpy = jest.spyOn(logger, 'log').mockImplementation();
+
+  beforeEach(() => {
+    storeSaveState(baseState as any);
+  });
+
+  afterEach(() => {
+    getAccountInfoSpy.mockClear();
+    logSpy.mockClear();
+  });
+
+  describe('account view - success path', () => {
+    test('✅ should print account details for requested account ID', async () => {
+      // Arrange
+      const program = new Command();
+      commands.accountCommands(program);
+
+      // Act
+      await program.parseAsync([
+        'node',
+        'hedera-cli.ts',
+        'account',
+        'view',
+        '-i',
+        accountResponse.account,
+      ]);
+
+      // Assert
+      expect(getAccountInfoSpy).toHaveBeenCalledWith(accountResponse.account);
+      expect(logSpy).toHaveBeenCalledWith(
+        `Account: ${accountResponse.account}`,
+      );
+      expect(logSpy).toHaveBeenCalledWith(
+        `Balance Tinybars: ${accountResponse.balance.balance}`,
+      );
+      expect(logSpy).toHaveBeenCalledWith(
+        `Deleted: ${accountResponse.deleted}`,
+      );
+      expect(logSpy).toHaveBeenCalledWith(
+        `EVM Address: ${accountResponse.evm_address}`,
+      );
+      expect(logSpy).toHaveBeenCalledWith(
+        `Key type: ${accountResponse.key._type} - Key: ${accountResponse.key.key}`,
+      );
+      expect(logSpy).toHaveBeenCalledWith(
+        `Max automatic token associations: ${accountResponse.max_automatic_token_associations}`,
+      );
+    });
   });
 });
-
-// const logger = Logger.getInstance();
-
-// describe('account view command', () => {
-//   const getAccountInfoSpy = jest
-//     .spyOn(api.account, 'getAccountInfo')
-//     .mockResolvedValue({
-//       data: accountResponse,
-//     });
-//   const logSpy = jest.spyOn(logger, 'log').mockImplementation();
-
-//   beforeEach(() => {
-//     storeSaveState(baseState as any);
-//   });
-
-//   afterEach(() => {
-//     getAccountInfoSpy.mockClear();
-//     logSpy.mockClear();
-//   });
-
-//   describe('account view - success path', () => {
-//     test('✅ should print account details for requested account ID', async () => {
-//       // Arrange
-//       const program = new Command();
-//       commands.accountCommands(program);
-
-//       // Act
-//       await program.parseAsync([
-//         'node',
-//         'hedera-cli.ts',
-//         'account',
-//         'view',
-//         '-i',
-//         accountResponse.account,
-//       ]);
-
-//       // Assert
-//       expect(getAccountInfoSpy).toHaveBeenCalledWith(accountResponse.account);
-//       expect(logSpy).toHaveBeenCalledWith(
-//         `Account: ${accountResponse.account}`,
-//       );
-//       expect(logSpy).toHaveBeenCalledWith(
-//         `Balance Tinybars: ${accountResponse.balance.balance}`,
-//       );
-//       expect(logSpy).toHaveBeenCalledWith(
-//         `Deleted: ${accountResponse.deleted}`,
-//       );
-//       expect(logSpy).toHaveBeenCalledWith(
-//         `EVM Address: ${accountResponse.evm_address}`,
-//       );
-//       expect(logSpy).toHaveBeenCalledWith(
-//         `Key type: ${accountResponse.key._type} - Key: ${accountResponse.key.key}`,
-//       );
-//       expect(logSpy).toHaveBeenCalledWith(
-//         `Max automatic token associations: ${accountResponse.max_automatic_token_associations}`,
-//       );
-//     });
-//   });
-// });
